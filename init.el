eb6ee93bd0174d357dcc1e87dfae167bdf2371d5
Osamu Ogasawara <osamu.ogasawara@gmail.com>
2020-11-05 19:17:26 +0900

    Edit comments in init.el

--- init.el
+++ init.el
@@ -6,27 +6,20 @@
 ;; You may delete these explanatory comments.
 (require 'package)
 
-;; MELPAを追加
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
-;; MELPA-stableを追加
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
-;; Marmaladeを追加
 ;;(add-to-list 'package-archives  '("marmalade" . "http://marmalade-repo.org/packages/") t)
-
-;; Orgを追加
 ;;(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
-;; 初期化
+
 (package-initialize)
 ;;(package-refresh-contents)
 
 
 (load-theme 'wombat)
 ;;(load-theme 'whiteboard)
-
-;; Here's list of emacs 24.3 themes.
 
 ;; adwaita
 ;; deeper-blue
@@ -42,21 +35,14 @@
 ;; whiteboard
 ;; wombat
 
-(load-file "~/.emacs.d/terminal-bot.el")
 (load-file "~/.emacs.d/bs.el")
 (load-file "~/.emacs.d/buffer-move.el")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;    Configuration file for Emacs 
-;;      Osamu Ogasawara (since 2001/05/24)
-;;      2012.05.08 (for Emacs v.23)
-;;
-;; 
-;;--- .emacs ---
-;;(setq load-path
-;;      (append (list nil "/home/oogasawa/dot-files")
-;;              load-path))
-;;(load "oga.el")
+;;      Osamu Ogasawara 
+;;      2020.11.05 (for Emacs v.26)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
