0fac7d605443063c33876c9b1045a1ccc203dfec
Osamu Ogasawara <oogasawa@nig.ac.jp>
2021-03-09 16:59:17 +0900

    Add elpa directory for the need of old tide package

---
+++ elpa/ctags-update-1.1/ctags-update-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ctags-update" "1.1" "(auto) update TAGS in parent directory using exuberant-ctags" 'nil :commit "67faf248b92388442958a069263c62a345425a1b" :authors '((nil . "Joseph(纪秀峰)  jixiuf@gmail.com")) :maintainer '(nil . "Joseph(纪秀峰)  jixiuf@gmail.com") :keywords '("exuberant-ctags" "etags") :url "https://github.com/jixiuf/ctags-update")
